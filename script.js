fd4b67298a1235d0d5b36a7ce8d29b52d772f604
aldiswili-lgtm <aldiswili@gmail.com>
2025-10-21 13:32:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function() {
-  alert("Halo! Terimakasih Sudah Berkunjung!');
+  alert('Halo! Terimakasih Sudah Berkunjung!');
 });
